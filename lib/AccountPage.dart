cd164944a3e9445ef214aa5a9fa703bff7638de7
Farah Khan Brinto <113084261+Fkhan684@users.noreply.github.com>
2024-04-01 10:53:38 -0500

    Update AccountPage.dart
    logout works

--- lib/AccountPage.dart
+++ lib/AccountPage.dart
@@ -20,138 +20,76 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    Size screenSize = MediaQuery.of(context).size;
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Accounts'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Accounts'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.white, // Background color for the whole page
         ),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'User Name',
-                style: TextStyle(
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                ),
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'User Account',
+              style: TextStyle(
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
               ),
-                Padding(padding: const EdgeInsets.only(top: 20.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Respond to button press
-                  },
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(screenSize.width, 50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(1.0),
-                    ),
-                  ),
-                  child: const Text('Acheivements'),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Respond to button press
+                },
+                child: Text('Achievements'),
               ),
-                ),
-              Padding(padding: const EdgeInsets.only(top: 20.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Respond to button press
-                  },
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(screenSize.width, 50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(1.0),
-                    ),
-                ),
-                  child: const Text('Wallet'),
-
-
-                  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Respond to button press
+                },
+                child: Text('Wallet'),
               ),
-
-
-              Padding(padding: const EdgeInsets.only(top: 20.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Respond to button press
-                  },
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(screenSize.width, 50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(1.0),
-                    ),
-                  ),
-                  child: const Text('change username/password'),
-
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Respond to button press
+                },
+                child: Text('Change Username/Password'),
               ),
-              Padding(padding: const EdgeInsets.only(top: 20.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Respond to button press
-                  },
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(screenSize.width, 50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(1.0),
-                    ),
-                  ),
-                  child: const Text('log out'),
-
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  signOut(context);
+                },
+                child: Text('Log Out'),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: const Text('About Us'),
-                            content: const SingleChildScrollView(
-                              child: ListBody(
-                                children: <Widget>[
-                                  Text('This is a betting app'),
-                                  Text('This app is for entertainment purposes only'),
-                                  Text('This app is not responsible for any losses'),
-                                  Text('This app is not responsible for any addiction'),
-                                  Text('Made by Group Lark')
-                                ],
-                              ),
-
-                            ),
-                            actions: <Widget>[
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                child: const Text('Close'),
-                              ),
-                            ],
-                          );
-                        });
-                  },
-
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(screenSize.width, 50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(1.0),
-                    ),
-                  ),
-                  child: const Text('About Us'),
-                ),
-              )
-            ]
-          )
-
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Respond to button press
+                },
+                child: Text('About Us'),
+              ),
+            )
+          ],
         ),
       ),
     );
   }
 }
-
 
 void main() {
   runApp(MaterialApp(
@@ -159,6 +97,4 @@ void main() {
       child: AccountPage(),
     ),
   ));
-
 }
-
