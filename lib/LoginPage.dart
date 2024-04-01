8238450ec5efb4c1bd77b436cabc3776ddd6d193
Farah Khan Brinto <113084261+Fkhan684@users.noreply.github.com>
2024-04-01 10:54:12 -0500

    Update LoginPage.dart
    log in works

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -4,7 +4,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:larkcoins/firebase_options.dart';
 import 'BottomNavigation.dart';
-import 'dbHandler.dart';
 import 'sign_up_page.dart';
 
 
