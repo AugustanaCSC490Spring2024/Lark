bc369eecd75f7d07e1b67731d30f1e76fed83c96
Shazil Farhan <shazilfarhan21@augustana.edu>
2024-04-09 09:17:53 -0500

    Merge changes

--- lib/IncompleteBets.dart
+++ lib/IncompleteBets.dart
@@ -3,8 +3,4 @@ import 'Bets.dart';
 class Incomplete extends Bets{
   Incomplete( super.city, super.date, super.predictedTempLow, super.predictedTempHigh, super.wager, super.expectedEarning);
 
-  
-
-
-  
 }
