427d9bd4af642605ce726859787034bfbe6d910b
Anubhav Rayamajhi <anubhav12331@gmail.com>
2024-04-09 08:53:27 -0500

    More changes on bets class

--- lib/IncompleteBets.dart
+++ lib/IncompleteBets.dart
@@ -1,9 +1,9 @@
 import 'Bets.dart';
 
-class Incomplete extends Bets{
-  Incomplete(super.userid, super.city, super.date, super.predictedTempLow, super.predictedTempHigh, super.complete, super.wager, super.expectedEarning);
+class IncompleteBets extends Bets{
+  IncompleteBets(super.userid, super.city, super.date, super.predictedTempLow, super.predictedTempHigh, super.complete, super.wager, super.expectedEarning);
 
-  
+
 
 
   
