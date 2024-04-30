25f71e5ecdb551fb7145f5a703afbbb7bc9a387f
Shazil Farhan <shazilfarhan21@augustana.edu>
2024-04-30 10:11:11 -0500

    Improved UI for home page

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:larkcoins/CompleteBets.dart';
 import 'package:larkcoins/dbHandler.dart';
 import 'Bets.dart';
+import 'IncompleteBets.dart';
 import 'logo.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,12 +11,11 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: CustomAppBar(
         leading: TopLeftLogo(),
-
       ),
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [Colors.lightBlue.shade50, Colors.white],
+            colors: [Colors.indigo.shade900, Colors.indigo.shade700],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
             stops: [0.0, 0.5],
@@ -26,66 +26,62 @@ class HomePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(16.0),
               child: Text(
                 'Your Bets',
                 style: TextStyle(
-                  fontSize: 20,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(height: 8),
+            _buildBetList("Pending Bets", getIncompleteBets()),
+            SizedBox(height: 16),
+            _buildBetList("Completed Bets", getCompletBets()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBetList(String title, Future<List<Bets>> betFuture) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                title,
+                style: TextStyle(
+                  fontSize: 18,
                   fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-                        Text("Pending Bets"),
-
             Expanded(
               child: FutureBuilder<List<Bets>>(
-                future: getIncompleteBets(),                
+                future: betFuture,
                 builder: (context, snapshot) {
                   if (snapshot.connectionState == ConnectionState.waiting) {
                     return Center(child: CircularProgressIndicator());
                   } else if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
+                    return Center(child: Text('Error: ${snapshot.error}'));
                   } else {
                     var bets = snapshot.data!;
                     return ListView.builder(
                       itemCount: bets.length,
                       itemBuilder: (context, index) {
-                        return Card(
-                          color: Color(0xFFE3F2FF), // Set background color here
-                          child: ListTile(
-                            title: Text(bets[index].zipCode),
-                            subtitle: Text("Amount: \$${bets[index].wager.toString()}"),
-                            trailing: Text("Expected: \$${bets[index].expectedEarning.toString()}"),
-                          ),
-                        );
-                      },
-                    );
-                  }
-                },
-              ),
-            ),
-
-            Text("Completed Bets"),
-            Expanded(
-              child: FutureBuilder<List<CompleteBets>>(
-                future: getCompletBets(),                
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
-                  } else {
-                    var bets = snapshot.data!;                    
-                    return ListView.builder(
-                      itemCount: bets.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          color: bets[index].result == true ? Color(0xFF00FF00) : Color(0xFFFF0000), // Set background color based on the result
-                          child: ListTile(
-                            title: Text(bets[index].zipCode),
-                            subtitle: Text("Amount: \$${bets[index].wager.toString()}"),
-                            trailing: Text("Expected: \$${bets[index].expectedEarning.toString()}"),
-                          ),
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                          child: BetCard(bet: bets[index]),
                         );
                       },
                     );
@@ -99,3 +95,77 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+class BetCard extends StatefulWidget {
+  final Bets bet;
+
+  const BetCard({Key? key, required this.bet}) : super(key: key);
+
+  @override
+  _BetCardState createState() => _BetCardState();
+}
+
+class _BetCardState extends State<BetCard> {
+  bool _isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    Color cardColor = Colors.blue;
+    String titleText = "";
+    Widget? additionalInfo;
+
+    if (widget.bet is IncompleteBets) {
+      IncompleteBets incompleteBet = widget.bet as IncompleteBets;
+      titleText = incompleteBet.zipCode;
+      additionalInfo = _buildAdditionalInfo(incompleteBet);
+    } else if (widget.bet is CompleteBets) {
+      CompleteBets completeBet = widget.bet as CompleteBets;
+      titleText = completeBet.zipCode;
+      additionalInfo = _buildAdditionalInfo(completeBet);
+      cardColor = completeBet.result ? Colors.green : Colors.red;
+    }
+
+    return Card(
+      color: cardColor,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      elevation: 3,
+      child: ExpansionTile(
+        title: Text(
+          titleText,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        onExpansionChanged: (value) {
+          setState(() {
+            _isExpanded = value;
+          });
+        },
+        children: [
+          additionalInfo ?? SizedBox.shrink(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAdditionalInfo(Bets bet) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 8),
+        ListTile(
+          leading: Icon(Icons.access_time, size: 16),
+          title: Text("Time: ${bet.date}", style: TextStyle(fontSize: 14)),
+        ),
+        ListTile(
+          leading: Icon(Icons.attach_money, size: 16),
+          title: Text("Expected Wins: \$${bet.expectedEarning}", style: TextStyle(fontSize: 14)),
+        ),
+        if (bet is IncompleteBets)
+          ListTile(
+            leading: Icon(Icons.thermostat, size: 16),
+            title: Text("Temperature: ${(bet as IncompleteBets).predictedTemp}", style: TextStyle(fontSize: 14)),
+          ),
+      ],
+    );
+  }
+}
+
